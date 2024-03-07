f88b9b5c46fd45d8241165cf34f868136020d118
viacheslavbocharov <147165756+viacheslavbocharov@users.noreply.github.com>
2024-03-07 15:34:06 +0000

    add form, form validation, show order details and product after send form

--- js/app.js
+++ js/app.js
@@ -12,21 +12,112 @@ document.getElementById('products').addEventListener('click', event => {
   showProductInfo(categoryId, productId);
 });
 
+
+
 //При натисканні на “купити” з'являється повідомлення, 
 //що товар куплений і повернення у вихідний стан програми 
 //( коли відображається лише список категорій).
 
 
+
 document.getElementById('product').addEventListener('click', event => {
+  const productId = event.target.getAttribute('data-product');
+  const categoryId = event.target.getAttribute('data-category');
+
   if (event.target.closest('.btn')) {
     clearFieldProducts()
     clearFieldProduct()
     showMessageAfterBuy();
+    showFormAfterBuy(categoryId, productId)
   }
 
 });
 
-//Не пойму почему не работала такая конструкция:
-// document.getElementById('buy').addEventListener('click', () => {
-//   showMessageAfterBuy()
-// });
+// Модифицировать приложение интернет-магазин 
+
+// В информации товара - кнопка “купить”
+// При клике на “купить” ниже появляется форма оформления заказа с следующими полями:
+// ФИО покупателя
+// Город (выбор из списка)
+// Склад Новой почты для отправки
+// Наложенный платеж или оплата банковской картой
+// Количество приобретаемой продукции
+// Комментарий к заказу
+// Реализовать проверку всех пользовательских данных при подтверждении заказа- обязательные поля заполнены. 
+// В противном случае, выводить ошибку на страницу
+// Выводить информацию о заказе на страницу (информация о товаре и о доставке)
+
+
+
+document.addEventListener('click', event => {
+
+  const productId = event.target.getAttribute('data-product');
+  const categoryId = event.target.getAttribute('data-category');
+
+  if (event.target.classList.contains('btn-send')) {
+    const orderForm = document.forms.orderForm;
+
+    const orderSurname = orderForm.surname.value;
+    const orderName = orderForm.name.value;
+    const orderMidlename = orderForm.midlename.value;
+    const orderCity = orderForm.city.value;
+    const orderNewpost = orderForm.newpost.value;
+    const orderPayment = orderForm.payment.value;
+    const orderQty = orderForm.qty.value;
+    const orderDescription = orderForm.description.value;
+
+    const allFields = {
+      surname: orderSurname,
+      name: orderName,
+      midlename: orderMidlename,
+      city: orderCity,
+      post: orderNewpost,
+      payment: orderPayment,
+      qty: orderQty,
+      note: orderDescription
+    };
+
+    const mandatoryFields = {
+      surname: orderSurname,
+      name: orderName,
+      midlename: orderMidlename,
+      city: orderCity,
+      post: orderNewpost,
+      qty: orderQty
+    };
+
+    // Логика для проверки введения данных в форму.
+    const inputFields = document.querySelectorAll('input');
+
+    alertInputsIfNotFilled(inputFields);
+
+
+    if (isMandatoryFieldsFilled(mandatoryFields)) {
+      showBoughtProductInfo(categoryId, productId);
+      showDeliveryInfo(allFields);
+    } else {
+      alert('Please fill in all field');
+    }
+
+  }
+});
+
+// console.log('Surname:', orderSurname);
+// console.log('Name:', orderName);
+// console.log('Midlename:', orderMidlename);
+// console.log('City:', orderCity);
+// console.log('New Poste Branch:', orderNewpost);
+// console.log('Payment:', orderPayment);
+// console.log('Qty:', orderQty);
+// console.log('Description:', orderDescription);
+
+
+{/* <h2>Order details:</h2>
+<p>Surname: ${allFields.surname}</p>
+<p>Name: ${orderName}</p>
+<p>Midlename: ${orderMidlename}</p>
+<p>City: ${orderCity}</p>
+<p>Post Brunch: ${orderNewpost}</p>
+<p>Payment method: ${orderPayment}</p>
+<p>Qty: ${orderQty}</p>
+<p>Your notes: ${orderDescription}</p> */}
--- js/functions.js
+++ js/functions.js
@@ -43,6 +43,8 @@ function showProductInfo(categoryId, productId) {
   buyButton.setAttribute('type', 'button');
   buyButton.setAttribute('value', 'Buy');
   buyButton.setAttribute('id', 'buy');
+  buyButton.setAttribute('data-product', productId);
+  buyButton.setAttribute('data-category', categoryId);
   buyButton.classList.add('btn');
 
 
@@ -65,10 +67,142 @@ function clearFieldProduct() {
 function showMessageAfterBuy() {
 
   const parent = document.getElementById('info');
-  parent.innerHTML = `Congrats! You've bought the product`;
+  const newElement = document.createElement('div')
+  newElement.innerHTML = `Congrats! You've bought the product`;
+  parent.prepend(newElement);
 
   setTimeout(function () {
-    parent.innerHTML = '';
+    //newElement.innerHTML = '';
+    newElement.remove();
   }, 2000);
 
 };
+
+function showFormAfterBuy(categoryId, productId){
+  const parent = document.getElementById('info');
+  const newElement = document.createElement('form')
+  newElement.setAttribute('name', 'orderForm');
+  newElement.innerHTML = `
+  <p><input type="text" name="surname" placeholder="Surname*" /></p>
+  <p><input type="text" name="name" placeholder="Name*" /></p>
+  <p><input type="text" name="midlename" placeholder="Midlename" /></p>
+  <select name="city">
+    <option value="not selected">--select city--</option>
+    <option value="Odessa">Odessa</option>
+    <option value="New-York">New-York</option>
+    <option value="Toronto">Toronto</option></select
+  ><br />
+  <p>
+    <input type="text" name="newpost" placeholder="New Poste Branch*" />
+  </p>
+  <label><input type="radio" name="payment" value="Card" /> Card </label
+  ><br />
+  <label
+    ><input type="radio" name="payment" value="Post" checked/> At Post
+    Branch</label
+  ><br />
+  <p><input type="number" name="qty" placeholder="Qty" min="0" /></p>
+  <p>
+    <textarea
+      name="description"
+      placeholder="Enter your comments"
+    ></textarea>
+  </p>
+  <p><input type="button" class="btn-send" data-product="${productId}" data-category="${categoryId}" value="Send" /></p>
+  `;
+  parent.append(newElement);
+};
+
+
+function alertInputsIfNotFilled(inputFields) {
+  inputFields.forEach(input => {
+    if (!input.value) {
+      // Если поле не заполнено, добавляем div с сообщением об ошибке
+      if (!input.nextElementSibling || input.nextElementSibling.className !== 'error-message') {
+        const errorMessage = document.createElement('div');
+        errorMessage.className = 'error-message';
+        errorMessage.textContent = 'This is a required field';
+        input.parentNode.insertBefore(errorMessage, input.nextElementSibling);
+      }
+    } else {
+      // Если поле заполнено, удаляем сообщение об ошибке (если оно было)
+      if (input.nextElementSibling && input.nextElementSibling.className === 'error-message') {
+        input.nextElementSibling.remove();
+      }
+    }
+  });
+};
+
+
+
+function isMandatoryFieldsFilled(mandatoryFields) {
+  for (const key in mandatoryFields) {
+    if (!mandatoryFields[key] && mandatoryFields[key] !== 0) {
+      // Если значение пусто или undefined, возвращаем false
+      return false;
+    }
+  }
+  // Если все значения не пустые, возвращаем true
+  return true;
+};
+
+
+function showBoughtProductInfo(categoryId, productId) {
+  const selectedCategory = data.find(category => category.key === categoryId);
+  const selectedProduct = selectedCategory.products.find(product => product.id == productId);
+
+  const parent = document.getElementById('info');
+  const productInfo = document.createElement('div')
+  
+  productInfo.innerHTML = `
+    <h2>Product:</h2>
+    <p>${selectedProduct.name}</p>
+    <p>Price: $${selectedProduct.price}</p>
+    <p>${selectedProduct.description}</p>
+  `;
+  parent.innerHTML = '';
+  parent.prepend(productInfo);
+}
+
+function showDeliveryInfo(allFields) {
+  const parent = document.getElementById('info');
+  const deliveryInfo = document.createElement('div')
+  //parent.innerHTML = '';
+  deliveryInfo.innerHTML = `
+<h2>Order details:</h2>
+<p>Surname: ${allFields.surname}</p>
+<p>Name: ${allFields.name}</p>
+<p>Midlename: ${allFields.midlename}</p>
+<p>City: ${allFields.city}</p>
+<p>Post Brunch: ${allFields.post}</p>
+<p>Payment method: ${allFields.payment}</p>
+<p>Qty: ${allFields.qty}</p>
+<p>Your notes: ${allFields.note}</p>
+`;
+  parent.append(deliveryInfo);
+};
+
+
+
+// function showDeliveryInfo() {
+//   const parent = document.getElementById('info');
+//   const deliveryInfo = document.createElement('div')
+//   //parent.innerHTML = '';
+//   deliveryInfo.innerHTML = `
+// <h2>Order details:</h2>
+// <p>Surname: ${orderSurname}</p>
+// <p>Name: ${orderName}</p>
+// <p>Midlename: ${orderMidlename}</p>
+// <p>City: ${orderCity}</p>
+// <p>Post Brunch: ${orderNewpost}</p>
+// <p>Payment method: ${orderPayment}</p>
+// <p>Qty: ${orderQty}</p>
+// <p>Your notes: ${orderDescription}</p>
+// `;
+//   parent.append(deliveryInfo);
+// };
+
+
+
+
+
